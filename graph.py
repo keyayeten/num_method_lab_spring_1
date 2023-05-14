4fe13e4f61068f0446cefde952ab75db7a758cfa
keyayeten <76627566+keyayeten@users.noreply.github.com>
2023-05-15 00:35:16 +0300

    1

--- graph.py
+++ graph.py
@@ -11,10 +11,11 @@ with open('main.txt', 'r') as f:
 results = [i.replace('\n', '').replace(' ', '').split(',')
            for i in results][1:-1:]
 results = [list(map(float, i)) for i in results]
-print(results)
 
-df = pd.DataFrame(results, columns=['N', 'Xi', 'V(xi)', 'V2(x2i)', 'V(xi) - V2(x2i)'])
+df = pd.DataFrame(results, columns=['N', 'Xi', 'V(xi)',
+                                    'V2(x2i)', 'V(xi) - V2(x2i)'])
 print(df)
 
 sns.regplot(data=df, x='Xi', y='V(xi)')
+sns.regplot(data=df, x='Xi', y='V2(x2i)')
 plt.show()
